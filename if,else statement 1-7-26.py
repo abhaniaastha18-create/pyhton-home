20008b4c627576b2dc1cef5526e3ef080950359f
aastha_abhani_18 <abhaniaastha18@gmail.com>
2026-07-02 00:18:42 +0530

    Add files via upload

---
+++ if,else statement 1-7-26.py
@@ -0,0 +1,23 @@
+#if else statement:
+age=20
+if (age>=18):
+    print("adult")
+else :
+    ("minor")
+
+
+age=17
+if (age>=18):
+    print("adult")
+else:
+    ("minor")
+
+
+#if ...elif....else statement :
+    #voting program:
+age=18
+card=true
+if (age>=18):
+       print ("you are eligible for vote ")
+       
+    
