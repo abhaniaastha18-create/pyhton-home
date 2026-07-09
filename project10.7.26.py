6cf29e6ec84ade181dbc17c21f41f4ffae52d2d1
aastha_abhani_18 <abhaniaastha18@gmail.com>
2026-07-10 01:23:19 +0530

    Add files via upload

---
+++ project10.7.26.py
@@ -0,0 +1,28 @@
+#detailed use print & input functions:
+print("personal information")
+name = input("enter your name")
+age =input ("enter your age")
+height =input("enter your height")
+fav_number= input("enter your fav num")
+print("personal information")
+
+
+#data types,variables & operators :
+current_year = 2026
+birth_year=present_year - age
+print("birth_year")
+
+print("details")
+name=(f"name :{name}")
+age=(f"age :{age}")
+height =(f"fav_num :{fav_num}")
+print("details")
+
+
+#casting example:
+print ("personal information")
+name=input("enter your name:")
+age =input ("enter your age")
+height =input("enter your height")
+fav_number=input("enter your fav ")
+
