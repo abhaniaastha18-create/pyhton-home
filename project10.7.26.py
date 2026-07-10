c0fa4c353c3b53b9acb4ebf82f37f096310e6f95
aastha_abhani_18 <abhaniaastha18@gmail.com>
2026-07-10 18:49:42 +0530

    Add files via upload

--- project10.7.26.py
+++ project10.7.26.py
@@ -6,16 +6,15 @@ height =input("enter your height")
 fav_number= input("enter your fav num")
 print("personal information")
 
+#birth year calculation:
+#current_year=2026
+#birth_year=current_year-age
+#print("\nyour birth year is approximately:",
+#birth_year,
+       # "(based on your age of",age,")")
+
 
 #data types,variables & operators :
-age="21"
-age=int(num)
-print(num)
-print(type(num))
-
-current_year = 2026
-birth_year=current_year - age
-print("birth_year")
 
 print("details")
 print=(f"name :{name}")
@@ -25,30 +24,67 @@ print("details")
 
 
 #casting example:
+print("user information")
 print ("personal information")
 name=input("enter your name:")
-age =int(input ("enter your age"))
-height =float(input("enter your height"))
+age =int(input ("enter your age"))#string to integer
+height =float(input("enter your height(in cm)"))#string to float
 fav_number=int(input("enter your fav "))
 
 #display original numbers:
-print("original num")
+print("user details")
 print(f"Name: {name}")
 print(f"Age:{age}(integer)")
 print(f"Height:{height}(float)")
-print(f"Fav_num:{fav_num}(integer)")
+print(f"Favourite_num:{fav_num}(integer)")
 
 #conversion:
+height_int=int(height)
+age_str=str(age)
+fav_float=float(fav_number)
 print(f"height converted from float to integer:{height_int}")
 print(f"age converted from integer to string:{age_string}")
 print(f"fav_num converted from integer to float :{fav_float}")
 
+#display function():
+print("user information")
+#taking input
+name=input("enter your name:")
+age=int(input("enter your age:"))
+height=float("enter your height (in cm):")
+fav_number=int(input("enter your favourite number:"))
+
+
+#display summary:
+print("variable summary")
+
+print("variable name:name")
+print("value   :",name)
+print("data type:",type(name))
+print("memory address:,id(name)")
+      
+      
+
+print("variable name:fav_num")
+print("value   :",fav_num)
+print("data type:",type(fav_num))
+print("memory address:,id(fav_num)")
+      
 
 
 
+print("variable name:age")
+print("value   :",age)
+print("data type:",type(age))
+print("memory address:,id(age)")
+      
 
 
-
+print("variable name:height")
+print("value   :",height)
+print("data type:",type(height))
+print("memory address:,id(height)")
+      
 
 
 
