56ee7871c51d75ff8d35bead7e7c39776a380cb0
aastha_abhani_18 <abhaniaastha18@gmail.com>
2026-07-10 06:46:28 +0530

    Add files via upload

--- project10.7.26.py
+++ project10.7.26.py
@@ -8,21 +8,47 @@ print("personal information")
 
 
 #data types,variables & operators :
+age="21"
+age=int(num)
+print(num)
+print(type(num))
+
 current_year = 2026
-birth_year=present_year - age
+birth_year=current_year - age
 print("birth_year")
 
 print("details")
-name=(f"name :{name}")
-age=(f"age :{age}")
-height =(f"fav_num :{fav_num}")
+print=(f"name :{name}")
+print=(f"age :{age}")
+print =(f"fav_num :{fav_num}")
 print("details")
 
 
 #casting example:
 print ("personal information")
 name=input("enter your name:")
-age =input ("enter your age")
-height =input("enter your height")
-fav_number=input("enter your fav ")
+age =int(input ("enter your age"))
+height =float(input("enter your height"))
+fav_number=int(input("enter your fav "))
+
+#display original numbers:
+print("original num")
+print(f"Name: {name}")
+print(f"Age:{age}(integer)")
+print(f"Height:{height}(float)")
+print(f"Fav_num:{fav_num}(integer)")
+
+#conversion:
+print(f"height converted from float to integer:{height_int}")
+print(f"age converted from integer to string:{age_string}")
+print(f"fav_num converted from integer to float :{fav_float}")
+
+
+
+
+
+
+
+
+
 
